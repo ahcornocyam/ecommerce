b050bf137a95258f616ac00e678b3f94702cbc81
Alexandre Roch <maycon.alex.rocha@hotmail.com>
2015-07-22 05:58:38 +0000

    primeiro commit

---
+++ app/Http/Kernel.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace ecommerce\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \ecommerce\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \ecommerce\Http\Middleware\VerifyCsrfToken::class,
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'auth' => \ecommerce\Http\Middleware\Authenticate::class,
+        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'guest' => \ecommerce\Http\Middleware\RedirectIfAuthenticated::class,
+    ];
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('app')
+@section('content')
+    <div class="container">
+        <div class="content">
+            <div class="title"><h1 class="well-lg text-capitalize text-center">E-Commerce</h1></div>
+        </div>
+    </div>
+@endsection
